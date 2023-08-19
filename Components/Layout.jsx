66e9995317bbc03e65c5fc499ca1e2c7423de5a9
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-08-19 00:24:33 -0300

    meta

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -8,6 +8,7 @@ export default function Layout({ children, title = 'Mi Web' }) {
   return (
     <div>
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         {/* Aquí puedes agregar más elementos al head como meta tags, etc. */}
       </Head>
